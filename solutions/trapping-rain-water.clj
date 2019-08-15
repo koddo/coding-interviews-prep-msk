62375542d26db211b8102d3500a20876542aba74
koddo <alex@egotv.ru>
2019-08-15 16:33:42 +0300

    add a solution for the trapping-rain-water problem
    https://leetcode.com/problems/trapping-rain-water/

---
+++ solutions/trapping-rain-water.clj
@@ -0,0 +1,28 @@
+;;; https://leetcode.com/problems/trapping-rain-water/
+
+;; with reduce
+(defn trap-rain-water [h]
+  (let [max-pos (first (apply max-key second (map-indexed vector h)))
+        [left-h  right-h] (split-at max-pos h)
+        f (fn([[m v] x] (if (> x m) [x v] [m (+ v (- m x))])))]
+    (+ (second (reduce f [0 0] left-h))
+       (second (reduce f [0 0] (reverse right-h))))))
+ 
+;; with loops
+(defn trap-rain-water [h]
+  (let [enumerated (map-indexed vector h)
+        max-pos (first (apply max-key second enumerated))]
+    (+ (loop [m 0 res 0 l enumerated]
+         (let [[pos x] (first l)]
+           (if (= pos max-pos)
+             res
+             (if (> x m)
+               (recur x res (rest l))
+               (recur m (+ res (- m x)) (rest l))))))
+       (loop [m 0 res 0 l (reverse enumerated)]
+         (let [[pos x] (first l)]
+           (if (= pos max-pos)
+             res
+             (if (> x m)
+               (recur x res (rest l))
+               (recur m (+ res (- m x)) (rest l)))))))))
